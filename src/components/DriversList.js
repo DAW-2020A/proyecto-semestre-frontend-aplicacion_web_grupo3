cb848ab13752522e4de10f50fa3ded0911ea4b22
Freddy_Calahorrano <49247953+FreddyAlejandroCalahorrano@users.noreply.github.com>
2020-09-25 13:32:30 -0500

    #7 Pagina de ver Conductor

--- src/components/DriversList.js
+++ src/components/DriversList.js
@@ -5,84 +5,91 @@ import { Link } from 'react-router-dom';
 import { useDriversList } from '../data/useDriversList';
 import ShowError from './ShowError';
 import {useCooperativaList} from "../data/useCooperativaList";
-
+import ReactDOM from 'react';
 const { Text } = Typography;
+const EditableContext = React.createContext();
+
+const EditableRow = ({ index, ...props }) => {
+    const [form] = Form.useForm();
+    return (
+        <Form form={form} component={false}>
+            <EditableContext.Provider value={form}>
+                <tr {...props} />
+            </EditableContext.Provider>
+        </Form>
+    );
+};
+
 
 
 const DriversList = ( props ) => {
 
-    const { drivers, isLoading, isError, mutate } = useDriversList();
+    const {drivers, isLoading, isError, mutate} = useDriversList();
     const columns = [
         {
-            title: 'Name',
+            title: 'Nombre',
             dataIndex: 'name',
 
         },
         {
-            title: 'Age',
-            dataIndex: 'age',
+            title: 'Apellido',
+            dataIndex: 'last_name',
         },
         {
-            title: 'Address',
-            dataIndex: 'address',
+            title: 'Celular',
+            dataIndex: 'cellphone',
+        },
+        {
+            title: 'Teléfono',
+            dataIndex: 'phone',
+        },
+        {
+            title: 'Direcíon',
+            dataIndex: 'direction',
+        },
+        {
+            title: 'e-mail',
+            dataIndex: 'mail',
         },
     ];
 
+
     const data = [
 
-        {
-            key: '2',
-            name: 'drivers.name',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-        },
+
         {
             key: '3',
             name: 'Joe Black',
             age: 32,
             address: 'Sidney No. 1 Lake Park',
         },
+
+
     ];
-
-    if( isLoading ) {
-      return <Row justify='center' gutter={ 30 }>
-        {
-          [ ...new Array( 9 ) ].map( ( _, i ) =>
-            <Col xs={ 24 } sm={ 12 } md={ 8 } style={ { marginBottom: 30 } } key={ i }>
-              <div style={ { textAlign: 'center' } }>
-                <Skeleton.Image style={ { width: 200 } } />
-                <Card title='' extra='' cover='' loading />
-              </div>
-            </Col>
-          )
-        }
-      </Row>;
+    if (isLoading) {
+        return <Row justify='center' gutter={30}>
+            {
+                [...new Array(9)].map((_, i) =>
+                    <Col xs={24} sm={12} md={8} style={{marginBottom: 30}} key={i}>
+                        <div style={{textAlign: 'center'}}>
+                            <Skeleton.Image style={{width: 200}}/>
+                            <Card title='' extra='' cover='' loading/>
+                        </div>
+                    </Col>
+                )
+            }
+        </Row>;
     }
 
-    if( isError ) {
-      return <ShowError error={ isError } />;
+    if (isError) {
+        return <ShowError error={isError}/>;
     }
-
     return (
-      <>
-          <Table columns={columns}  size="middle" >
+        <>
+            <Table columns={columns} dataSource={drivers}></Table>
 
-              {
-                  drivers.map( ( driver, i ) => (
-                      <col>{driver.name}</col>
-                  ) )
-              }
-          </Table>
-        <ul>
-          {
-            drivers.map( ( driver, i ) => (
-              <li>{driver.name}</li>
-            ) )
-          }
-        </ul>
-      </>
+        </>
     );
-  }
-;
 
+};
 export default DriversList;
